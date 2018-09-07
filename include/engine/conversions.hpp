b502e2bc80935d447057bf880593953d8b36df4f
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-09-07 14:11:15 +0100

    Pulled NifLoaderState into its own file

--- include/engine/conversions.hpp
+++ include/engine/conversions.hpp
@@ -8,29 +8,29 @@
 
 namespace engine::conversions {
 
-Ogre::Vector3 fromNif(const nif::compound::Vector3 &v) {
+inline Ogre::Vector3 fromNif(const nif::compound::Vector3 &v) {
   return Ogre::Vector3{v.x, v.y, v.z};
 }
 
-Ogre::Matrix3 fromNif(const nif::compound::Matrix33 &m) {
+inline Ogre::Matrix3 fromNif(const nif::compound::Matrix33 &m) {
   return Ogre::Matrix3{m.m11, m.m12, m.m13,
                        m.m21, m.m22, m.m23,
                        m.m31, m.m32, m.m33};
 }
 
-Ogre::ColourValue fromNif(const nif::compound::Color3 &c) {
+inline Ogre::ColourValue fromNif(const nif::compound::Color3 &c) {
   return Ogre::ColourValue(c.r, c.g, c.b);
 }
 
-Ogre::ColourValue fromNif(const nif::compound::Color4 &c) {
+inline Ogre::ColourValue fromNif(const nif::compound::Color4 &c) {
   return Ogre::ColourValue(c.r, c.g, c.b, c.a);
 }
 
-Ogre::Vector3 fromBSCoordinates(const Ogre::Vector3 &v) {
+inline Ogre::Vector3 fromBSCoordinates(const Ogre::Vector3 &v) {
   return {v.x, v.z, -v.y};
 }
 
-Ogre::Matrix3 fromBSCoordinates(const Ogre::Matrix3 &m) {
+inline Ogre::Matrix3 fromBSCoordinates(const Ogre::Matrix3 &m) {
   const Ogre::Matrix3 C = {1, 0, 0,
                            0, 0, 1,
                            0, -1, 0};
--- include/engine/nif_loader.hpp
+++ include/engine/nif_loader.hpp
@@ -1,6 +1,7 @@
 #ifndef OPENOBLIVION_ENGINE_NIF_LOADER_HPP
 #define OPENOBLIVION_ENGINE_NIF_LOADER_HPP
 
+#include "nif/basic.hpp"
 #include "nif/niobject.hpp"
 #include "nif/versionable.hpp"
 #include <boost/graph/adjacency_list.hpp>
@@ -12,6 +13,7 @@
 #include <memory>
 #include <ostream>
 #include <string>
+#include <type_traits>
 
 namespace engine {
 
@@ -66,114 +68,6 @@ class NifLoader : public Ogre::ManualResourceLoader {
   // Read the nif file from the input stream and dump to the output stream in
   // obj format. Used for debugging.
   void dumpAsObj(std::istream &in, std::ostream &out);
-};
-
-// When constructing the mesh we want to iterate over the block graph, but
-// because of references and pointers we will have to jump around and load
-// things out of order when needed. To detect cycles and ensure that some blocks
-// are only loaded once, we tag each block with a LoadStatus.
-class NifLoaderState {
- private:
-  // Used to tag blocks to keep track of loading.
-  enum class LoadStatus {
-    Unloaded,
-    Loading,
-    Loaded
-  };
-
-  // A block and its load status. Blocks can be implicitly promoted to
-  // unloaded TaggedBlocks, used in construction of the block graph.
-  struct TaggedBlock {
-    NifLoader::Block block{};
-    LoadStatus tag{LoadStatus::Unloaded};
-
-    // NOLINTNEXTLINE(google-explicit-constructor)
-    TaggedBlock(NifLoader::Block block) : block(std::move(block)) {}
-    // Required by BlockGraph
-    TaggedBlock() = default;
-  };
-
-  // Used for RAII management of block load status. Should be constructed with
-  // the tag of the block that is being loaded at the same scope of the block,
-  // so that it goes out of scope when the block has finished loading.
-  // Automatically detects cycles.
-  class Tagger {
-    LoadStatus &tag;
-   public:
-    explicit Tagger(LoadStatus &tag) : tag{tag} {
-      switch (tag) {
-        case LoadStatus::Unloaded: tag = LoadStatus::Loading;
-          break;
-        case LoadStatus::Loading:
-          throw std::runtime_error("Cycle detected while loading nif file");
-        default:break;
-      }
-    }
-    ~Tagger() {
-      tag = LoadStatus::Loaded;
-    }
-  };
-
-  using TaggedBlockGraph = boost::adjacency_list<boost::vecS,
-                                                 boost::vecS,
-                                                 boost::bidirectionalS,
-                                                 TaggedBlock>;
-
-  TaggedBlockGraph blocks;
-
-  struct BoundedSubmesh {
-    Ogre::SubMesh *submesh{};
-    Ogre::AxisAlignedBox bbox{};
-  };
-  BoundedSubmesh parseNiTriShape(nif::NiTriShape *block, LoadStatus &tag);
-
-  std::shared_ptr<Ogre::Material>
-  parseNiMaterialProperty(nif::NiMaterialProperty *block, LoadStatus &tag);
-
-  struct TextureFamily {
-    using TexturePtr = std::unique_ptr<Ogre::TextureUnitState>;
-    TexturePtr base{};
-    TexturePtr dark{};
-    TexturePtr detail{};
-    TexturePtr gloss{};
-    TexturePtr glow{};
-    // Bump textures are treated differently and we use normal maps anyway
-    /* TexturePtr bump{}; */
-    std::vector<TexturePtr> decals{};
-  };
-  // See parseTexDesc for why the pass is necessary.
-  TextureFamily
-  parseNiTexturingProperty(nif::NiTexturingProperty *block, LoadStatus &tag,
-                           Ogre::Pass *pass);
-
-  // When setting the texture name of a texture unit, Ogre looks up and loads
-  // the texture using the resource group of its parent. Thus contrary to what
-  // addTextureUnitState seems to suggest, one should not create a
-  // Ogre::TextureUnitState with a nullptr parent, and we have to supply the
-  // parent pass here.
-  std::unique_ptr<Ogre::TextureUnitState>
-  parseTexDesc(nif::compound::TexDesc *tex, Ogre::Pass *parent);
-
-  void parseNiSourceTexture(nif::NiSourceTexture *block, LoadStatus &tag,
-                            Ogre::TextureUnitState *tex);
-
-  // Reads vertex, normal, and texcoord data from NiTriShapeData and prepares it
-  // for rendering.
-  std::unique_ptr<Ogre::VertexData>
-  generateVertexData(nif::NiTriShapeData *block, Ogre::Matrix4 transformation);
-
-  // Reads triangle data from NiTriShapeData and prepares it for rendering.
-  std::unique_ptr<Ogre::IndexData>
-  generateIndexData(nif::NiTriShapeData *block);
-
-  Ogre::AxisAlignedBox getBoundingBox(nif::NiTriShapeData *block,
-                                      Ogre::Matrix4 transformation);
-
-  Ogre::Mesh *mesh;
-  Ogre::LogManager &logger{Ogre::LogManager::getSingleton()};
-
- public:
-  explicit NifLoaderState(Ogre::Mesh *mesh, NifLoader::BlockGraph blocks);
 };
 
 template<class T>
