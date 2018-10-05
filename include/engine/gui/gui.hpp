04f5a602671c27a173c7976a9bdff4ee1f104898
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-10-05 10:58:08 +0100

    Added trait selectors

--- include/engine/gui/gui.hpp
+++ include/engine/gui/gui.hpp
@@ -5,6 +5,8 @@
 #include "engine/gui/xml.hpp"
 #include <boost/graph/adjacency_list.hpp>
 #include <functional>
+#include <regex>
+#include <stdexcept>
 #include <type_traits>
 #include <unordered_map>
 #include <variant>
@@ -368,6 +370,71 @@ struct PersistentFunctor {
   }
 };
 
+// Trait selectors look up the name of an element based on a rule and an
+// optional argument, like parent() or sibling(foo). They can be used instead
+// of trait names in operators.
+struct TraitSelector {
+  enum class Type {
+    child, last, me, parent, screen, sibling, strings
+  };
+  Type type{};
+  std::optional<std::string> argument{};
+};
+
+// Attempt to read the selector string as a TraitSelector
+std::optional<TraitSelector> tokenizeTraitSelector(std::string selector);
+
+// If an argument is given, then the 'child' selector returns the
+// fully-qualified name of a descendant of node, whose name matches the
+// argument, by performing a depth-first-search but iterating over the children
+// in reverse order. If no argument is given, then the selector returns the
+// fully-qualified name of the last non-trait child of node.
+std::string invokeChildSelector(const pugi::xml_node &node,
+                                std::optional<std::string> arg);
+
+// Not sure what this one is supposed to do, presumably it is not equivalent
+// to child(), which would return the last child of node. What it actually does
+// is return an empty string.
+std::string invokeLastSelector(const pugi::xml_node &node);
+
+// Return the fully-qualified name of the containing element.
+// See invokeSelector.
+std::string invokeMeSelector(const pugi::xml_node &node);
+
+// Return the fully-qualified name of the containing element's parent.
+// See invokeSelector.
+std::string invokeParentSelector(const pugi::xml_node &node);
+
+// screen is an implementation defined element describing screen dimensions
+// in normalized coordinates (NC). If screen width / screen height >= 1 then
+// the height is normalized to 960px and the width computed according to the
+// aspect ratio. Otherwise, the width is normalized to 1280px and the height
+// is computed according to the aspect ratio. screen has the following traits:
+//  - <width>: the screen width in NC
+//  - <height>: the screen height in NC
+//  - <cropX>: the horizontal safe zone margin width in NC
+//  - <cropY>: the vertical safe zone margin height in NC
+std::string invokeScreenSelector();
+
+// If an argument is given then return the fully-qualified name of the sibling
+// of the node whose name matches the argument. If no argument is given then
+// return the fully-qualified name of the sibling defined before the node.
+// This function is of the opinion that you are not your own sibling, calling
+// sibling(foo) inside foo will return an empty string, as will sibling() when
+// foo is an only child.
+std::string invokeSiblingSelector(const pugi::xml_node &node,
+                                  std::optional<std::string> arg);
+
+// strings.xml is used for localization purposes, each trait takes the value
+// of a localized string.
+std::string invokeStringsSelector();
+
+// Return whatever the selector selects, starting from node. It is expected that
+// node point to the containing parent element of the operator invoking the
+// selector, so usually one has to go at least one level up before calling this.
+std::string invokeSelector(const pugi::xml_node &node,
+                           const TraitSelector &selector);
+
 // Given a trait node whose body is given by a collection of operators, parse
 // the sequence of operators into a TraitFun.
 template<class T>
@@ -392,9 +459,17 @@ TraitFun<T> parseOperators(const Traits &traits, const pugi::xml_node &node) {
     auto srcAttr = copyNode.attribute("src");
     auto traitAttr = copyNode.attribute("trait");
     if (srcAttr && traitAttr) {
-      // TODO: Selectors and switch-case
-      auto name =
-          std::string{srcAttr.value()} + "." + std::string{traitAttr.value()};
+      std::string src{srcAttr.value()};
+      std::string trait{traitAttr.value()};
+      const auto selector = tokenizeTraitSelector(src);
+      if (selector) {
+        // node points to a trait and therefore has no non-operator children; need
+        // to go up another level to begin searching.
+        src = invokeSelector(node.parent(), *selector);
+      }
+
+      // TODO: switch-case
+      auto name = src + "." + trait;
       fun = [name, &tref = std::as_const(traits)]() -> FunctorPair {
         return {nullptr, tref.getTrait<T>(name).invoke()};
       };
--- src/engine/gui/gui.cpp
+++ src/engine/gui/gui.cpp
@@ -2,6 +2,7 @@
 #include "engine/gui/gui.hpp"
 #include "engine/gui/xml.hpp"
 #include "engine/settings.hpp"
+#include <boost/range/adaptors.hpp>
 #include <boost/graph/topological_sort.hpp>
 #include <pugixml.hpp>
 #include <spdlog/spdlog.h>
@@ -158,6 +159,159 @@ void parseMenu(std::istream &is) {
 
   // Force an update to initialize everything
   menuTraits.update();
+}
+
+std::optional<TraitSelector> tokenizeTraitSelector(std::string src) {
+  const std::regex selectorRegex{"(.+?)\\((.*?)\\)"};
+  std::smatch selectorMatch{};
+  std::regex_match(src, selectorMatch, selectorRegex);
+  if (selectorMatch.empty()) {
+    return std::nullopt;
+  }
+
+  TraitSelector parsedSelector{};
+
+  // First match is the entire string, second match is the selector name
+  const std::string selector{selectorMatch[1].str()};
+  using namespace std::literals;
+  if (selector == "child"s) {
+    parsedSelector.type = TraitSelector::Type::child;
+  } else if (selector == "last"s) {
+    parsedSelector.type = TraitSelector::Type::last;
+  } else if (selector == "me"s) {
+    parsedSelector.type = TraitSelector::Type::me;
+  } else if (selector == "parent"s) {
+    parsedSelector.type = TraitSelector::Type::parent;
+  } else if (selector == "screen"s) {
+    parsedSelector.type = TraitSelector::Type::screen;
+  } else if (selector == "sibling"s) {
+    parsedSelector.type = TraitSelector::Type::sibling;
+  } else if (selector == "strings"s) {
+    parsedSelector.type = TraitSelector::Type::strings;
+  }
+
+  // Third match is argument, if any
+  if (selectorMatch.size() == 3) {
+    parsedSelector.argument = selectorMatch[2].str();
+  }
+
+  return parsedSelector;
+}
+
+std::string invokeChildSelector(const pugi::xml_node &node,
+                                std::optional<std::string> arg) {
+  if (arg) {
+    // Search through this node's descendants for a node whose name matches the
+    // argument, but search from the last sibling to the first
+    for (const auto &child : node.children() | boost::adaptors::reversed) {
+      if (arg == child.attribute("name").value()) {
+        std::string parentName{node.attribute("name").value()};
+        std::string childName{child.attribute("name").value()};
+        return childName.append(".").append(parentName);
+      } else {
+        auto childName = invokeChildSelector(child, arg);
+        if (!childName.empty()) return childName;
+      }
+    }
+    return "";
+  } else {
+    // Just return this node's last child that has a name attribute; this
+    // ensures that we are not returning a trait.
+    for (const auto &child : node.children() | boost::adaptors::reversed) {
+      if (child.attribute("name")) {
+        std::string parentName{node.attribute("name").value()};
+        std::string childName{child.attribute("name").value()};
+        return childName.append(".").append(parentName);
+      }
+    }
+    // No valid children, so return nothing
+    return "";
+  }
+}
+
+std::string invokeLastSelector(const pugi::xml_node &node) {
+  // TODO: Warn that this is unimplemented
+  return "";
+}
+
+std::string invokeMeSelector(const pugi::xml_node &node) {
+  std::string parentName{node.parent().attribute("name").value()};
+  std::string childName{node.attribute("name").value()};
+  // <menu> elements have empty parents, in which case their fully-qualified
+  // name is just their name.
+  if (parentName.empty()) return childName;
+  else return parentName.append(".").append(childName);
+}
+
+std::string invokeParentSelector(const pugi::xml_node &node) {
+  std::string parentName{node.parent().attribute("name").value()};
+  std::string grandparentName{node.parent().parent().attribute("name").value()};
+  if (parentName.empty()) return "";
+    // If the parent is a <menu> then the grandparent is empty, and the
+    // fully-qualified parent name is just the parent name.
+  else if (grandparentName.empty()) return parentName;
+  else return grandparentName.append(".").append(parentName);
+}
+
+std::string invokeScreenSelector() {
+  return "__screen";
+}
+
+std::string invokeSiblingSelector(const pugi::xml_node &node,
+                                  std::optional<std::string> arg) {
+  auto parent = node.parent();
+  std::string parentName{parent.attribute("name").value()};
+  if (arg) {
+    // The unique sibling with the given name is the first child of the parent
+    // with that name, since names are unique.
+    if (parent.find_child_by_attribute("name", arg->c_str())) {
+      // sibling(me()) == "" contractually
+      if (*arg == node.attribute("name").value()) return "";
+      else return parentName.append(".").append(*arg);
+    }
+    return "";
+  } else {
+    // previous_sibling might return a trait, so we have to walk backwards and
+    // find the first non-trait.
+    for (auto sibling = node.previous_sibling(); sibling;
+         sibling = sibling.previous_sibling()) {
+      if (sibling.attribute("name")) {
+        return parentName.append(".").append(sibling.attribute("name").value());
+      }
+    }
+  }
+  return "";
+}
+
+std::string invokeStringsSelector() {
+  return "__strings";
+}
+
+std::string invokeSelector(const pugi::xml_node &node,
+                           const TraitSelector &selector) {
+  switch (selector.type) {
+    case TraitSelector::Type::child: {
+      return invokeChildSelector(node, selector.argument);
+    }
+    case TraitSelector::Type::last: {
+      return invokeLastSelector(node);
+    }
+    case TraitSelector::Type::me: {
+      return invokeMeSelector(node);
+    }
+    case TraitSelector::Type::parent: {
+      return invokeParentSelector(node);
+    }
+    case TraitSelector::Type::screen: {
+      return invokeScreenSelector();
+    }
+    case TraitSelector::Type::sibling: {
+      return invokeSiblingSelector(node, selector.argument);
+    }
+    case TraitSelector::Type::strings: {
+      return invokeStringsSelector();
+    }
+  }
 }
 
 namespace xml {
