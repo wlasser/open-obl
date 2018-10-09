a2744122a0ac79bf5ac847c07aa6705a149567eb
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-10-09 10:00:44 +0100

    Removed unnecessary allocation in readBzString
    I profiled, this is faster.

--- src/io/string.cpp
+++ src/io/string.cpp
@@ -1,26 +1,25 @@
 #include "io/string.hpp"
 
 std::string io::readBzString(std::istream &s) {
-  // Read length
-  uint8_t len = 0;
+  // len includes the null-terminator, which needs to be read, but reading len
+  // characters gives a string with an internal null which needs to be popped.
+  // This is faster than skipping the null or constructing from a char[]
+  // (which stops at the first null).
+  uint8_t len{0};
   s.read(reinterpret_cast<char *>(&len), 1);
-  // Easier to read into a char array then convert than into a string
-  auto arr = new char[len];
-  s.read(arr, len);
-  std::string str(arr);
-  delete[] arr;
+  if (len <= 1u) return "";
+  std::string str(len, '\0');
+  s.read(str.data(), len);
+  str.pop_back();
   return str;
 }
 
 std::string io::readBString(std::istream &s) {
-  // Much the same as readBzString but without the null-terminator so
-  // we add it ourselves
-  uint8_t len = 0;
+  // No null-terminator this time
+  uint8_t len{0};
   s.read(reinterpret_cast<char *>(&len), 1);
-  auto arr = new char[len + 1];
-  s.read(arr, len);
-  arr[len] = 0;
-  std::string str(arr);
-  delete[] arr;
+  if (len < 1u) return "";
+  std::string str(len, '\0');
+  s.read(str.data(), len);
   return str;
 }
