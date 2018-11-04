9ae7c29a75b322365bcab8fe85a84d8c54bda98b
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-11-04 00:28:43 +0000

    Fixed ImGui bindings, text is displayed now
    It'd be nice if the GLSL compiler warned when there are out parameters
    that are never used.

--- shaders/imgui_vs.glsl
+++ shaders/imgui_vs.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 attribute vec2 vertex;
-attribute vec2 uv;
 attribute vec4 colour;
+attribute vec4 uv0;
 
 out vec2 TexCoord;
 out vec4 VertexCol;
@@ -11,5 +11,6 @@ uniform mat4 worldViewProj;
 void main() {
     gl_Position = worldViewProj * vec4(vertex, 0.0f, 1.0f);
     VertexCol = colour;
+    TexCoord = uv0.xy;
 }
 
