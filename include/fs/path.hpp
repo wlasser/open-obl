a20f151379cb469a553e5a65de3c26c515b0b669
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-11-17 15:47:35 +0000

    Fixed formatting of Doxygen
    Doxygen interprets `'.'` as an empty TeX quoted empty string, followed
    by a . (which ends the brief description), then a single quote, then the
    start of a code span. `.` doesn't work either as the dot still ends the
    brief description. I think if autobrief is on then an explicit \brief
    should disable the autobrief behaviour, which (partially) solves this.

--- include/fs/path.hpp
+++ include/fs/path.hpp
@@ -12,17 +12,17 @@ namespace fs {
 
 /// Lightweight alternative to dealing with std::filesystem::path when paths are
 /// very simple. This class represents a case-insensitive file or directory path
-/// inside some unknown directory, with `'/'` and `'\\'` as the directory
-/// separators. `'.'` and `'..'` do not have their usual meanings; they are
-/// stripped from the beginning and end of the path along with any `'/'` and
+/// inside some unknown directory, with `/` and `\\` as the directory
+/// separators. `.` and `..` do not have their usual meanings; they are
+/// stripped from the beginning and end of the path along with any `/` and
 /// treated as any other character everywhere else in the path. For example,
-/// `'./foo\\.bar/..\\.'` and `'foo/.bar'` are the same. Every path is
+/// `./foo\\.bar/..\\.` and `foo/.bar` are the same. Every path is
 /// also assumed to contain only ascii characters.
 class Path {
  private:
   using size_type = std::string::size_type;
-  /// Lowercase normalized path with `'/'` separators and no trailing `'/'`
-  /// or `'.'`.
+  /// Lowercase normalized path with `/` separators and no trailing `/` or
+  /// `&period;`.
   /*const*/ std::string mPath{};
   mutable std::optional<std::filesystem::path> mSysPath{};
   mutable std::mutex mSysPathMutex{};
@@ -52,20 +52,20 @@ class Path {
   /// Store a normalized representation of the given path
   explicit Path(const std::string &path);
 
-  /// Return the part of the path after the last `'/'`, not including the `'/'`.
+  /// Return the part of the path after the last `/`, not including the `/`.
   /// This may not return an actual file, for example if the path points to a
-  /// folder. If there is no `'/'` then the entire path is returned.
+  /// folder. If there is no `/` then the entire path is returned.
   /// The returned path is always lowercase.
   std::string_view filename() const;
 
-  /// Return the complement of filename() without the trailing `'/'`.
+  /// Return the complement of filename() without the trailing `/`.
   /// The returned path is always lowercase.
   std::string_view folder() const;
 
-  /// Return the part of the filename strictly after the last '.' that does not
-  /// occur at the beginning of the filename, i.e. including the '.' itself.
+  /// Return the part of the filename strictly after the last `.\ ` that
+  /// does not occur at the beginning of the filename.
   /// Note that this is different to std::filesystem::path::extension, which
-  /// includes the '.'.
+  /// includes the `.`.
   std::string_view extension() const;
 
   /// Return true if this path refers to an existing file or folder. Because of
@@ -77,16 +77,16 @@ class Path {
   /// the same as this. If there are multiple such files/folders, the returned
   /// file/folder is unspecified, but will always be the same for repeated calls
   /// on the same Path.
-  /// \exception std::runtime_error Thro if no such file/folder exists.
+  /// \exception std::runtime_error Thrown if no such file/folder exists.
   std::filesystem::path sysPath() const;
 
   /// Return true if this path matches the given pattern.
-  /// A `'*'` in the pattern means to greedily match until a character equal to
-  /// the subsequent character in the pattern is found. If `'*'` occurs at the
+  /// A `*` in the pattern means to greedily match until a character equal to
+  /// the subsequent character in the pattern is found. If `*` occurs at the
   /// end of the pattern, then the rest of the path is matched automatically.
-  /// The subsequene `'**'` of a pattern acts consistenly; it will match every
-  /// character in the path until a literal '*' is found. There is no way to
-  /// explictly match for a literal `'*'`.
+  /// The subsequence `**` of a pattern acts consistently; it will match every
+  /// character in the path until a literal `*` is found. There is no way to
+  /// explicitly match for a literal `*`.
   bool match(const fs::Path &pattern) const;
 
   inline std::string_view view() const {
