391997d96e6ecff237c375d5299e1331a57082ca
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-11-18 00:24:35 +0000

    Made io test style project consistent

--- test/io/io.cpp
+++ test/io/io.cpp
@@ -6,36 +6,34 @@
 
 using std::string_literals::operator ""s;
 
-TEST_CASE("can write fundamental types", "[io]") {
+TEST_CASE("can write trivial types", "[io]") {
   std::ostringstream os{};
-  int i = 0x12345678;
+  const int i{0x12345678};
   io::writeBytes(os, i);
   REQUIRE(os.str() == "\x78\x56\x34\x12");
 }
 
 TEST_CASE("can write strings", "[io]") {
   std::ostringstream os{};
-  std::string s = "hello, world";
+  const std::string s{"hello, world"};
   io::writeBytes(os, s);
   REQUIRE(os.str() == "hello, world\0"s);
 }
 
 TEST_CASE("can write pairs", "[io]") {
   std::ostringstream os{};
-  int i = 0x12345678;
-  std::string s = "hello, world";
-  auto p = std::make_pair(i, s);
+  const std::pair<int, std::string> p{0x12345678, "hello, world"};
   io::writeBytes(os, p);
   REQUIRE(os.str() == "\x78\x56\x34\x12hello, world\0"s);
 }
 
 TEST_CASE("can write optionals", "[io]") {
   std::ostringstream os{};
-  std::optional<int> opt = 0x12345678;
+  const std::optional<int> opt{0x12345678};
   io::writeBytes(os, opt);
   REQUIRE(os.str() == "\x78\x56\x34\x12");
 
-  std::optional<int> emptyOpt = std::nullopt;
+  const std::optional<int> emptyOpt{std::nullopt};
   io::writeBytes(os, emptyOpt);
   REQUIRE(os.str() == "\x78\x56\x34\x12");
 }
