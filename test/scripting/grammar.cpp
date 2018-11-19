405d252c9d0eac707dde08d31afa89bab93feb47
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-11-19 10:42:39 +0000

    Refactored scriptname tests

--- test/scripting/grammar.cpp
+++ test/scripting/grammar.cpp
@@ -6,37 +6,7 @@
 
 namespace pegtl = tao::TAO_PEGTL_NAMESPACE;
 
-TEST_CASE("grammar is valid", "[scripting]") {
-  const auto numIssues{pegtl::analyze<scripting::Grammar>()};
-  REQUIRE(numIssues == 0);
-}
-
-void print_node(std::ostream &os,
-                const pegtl::parse_tree::node &node,
-                const std::string &indent = "") {
-  if (node.is_root()) {
-    os << "ROOT\n";
-  } else {
-    if (node.has_content()) {
-      os << indent
-         << node.name()
-         << " \"" << node.content() << "\" at "
-         << node.begin() << " to " << node.end()
-         << '\n';
-    } else {
-      os << indent
-         << node.name()
-         << " at "
-         << node.begin() << " to " << node.end()
-         << '\n';
-    }
-  }
-  for (const auto &child : node.children) {
-    print_node(os, *child, indent + "  ");
-  }
-}
-
-std::string
+[[nodiscard]] std::string
 printVisitor(const pegtl::parse_tree::node &node, const std::string &indent) {
   if (node.is_root()) {
     std::cout << "ROOT\n";
@@ -48,45 +18,76 @@ printVisitor(const pegtl::parse_tree::node &node, const std::string &indent) {
   return indent + "| ";
 };
 
+void printAst(const pegtl::parse_tree::node &node) {
+  scripting::visitAst(node, std::string{}, printVisitor);
+}
+
+template<class T> [[nodiscard]] auto parseScript(T &&in) {
+  return pegtl::parse_tree::parse<scripting::Grammar,
+                                  scripting::AstSelector>(in);
+}
+
+TEST_CASE("grammar is valid", "[scripting]") {
+  const auto numIssues{pegtl::analyze<scripting::Grammar>()};
+  REQUIRE(numIssues == 0);
+}
+
 TEST_CASE("can parse scriptname", "[scripting]") {
   const auto numIssues{pegtl::analyze<scripting::Grammar>()};
   REQUIRE(numIssues == 0);
 
-  {
+  std::unique_ptr<pegtl::parse_tree::node> root{};
+
+  SECTION("parse minimal scriptname") {
     const char *script = "scriptname MyScript";
     pegtl::memory_input in(script, "");
-    const auto root{pegtl::parse_tree::parse<scripting::Grammar,
-                                             scripting::AstSelector>(in)};
-
-    REQUIRE(root != nullptr);
-    REQUIRE(!root->children.empty());
-
-    const auto &scriptnameStatement{root->children[0]};
-    REQUIRE(!scriptnameStatement->children.empty());
-
-    const auto &scriptname{scriptnameStatement->children[1]};
-    REQUIRE(scriptname->has_content());
-    REQUIRE(scriptname->content() == "MyScript");
+    root = parseScript(in);
   }
 
-  {
-    const char *script = R"script(; First comment
+  SECTION("parse scriptname with surrounding comments") {
+    const char *script = R"script(
+; First comment
  scn MyScript ; This is the script name
     )script";
     pegtl::memory_input in(script, "");
-    const auto root{pegtl::parse_tree::parse<scripting::Grammar,
-                                             scripting::AstSelector>(in)};
+    root = parseScript(in);
+  }
 
-    REQUIRE(root != nullptr);
-    REQUIRE(!root->children.empty());
+  REQUIRE(root != nullptr);
+  REQUIRE(!root->children.empty());
 
-    const auto &scriptnameStatement{root->children[0]};
-    REQUIRE(!scriptnameStatement->children.empty());
+  const auto &scriptnameStatement{root->children[0]};
+  REQUIRE(scriptnameStatement->children.size() > 1);
 
-    const auto &scriptname{scriptnameStatement->children[1]};
-    REQUIRE(scriptname->has_content());
-    REQUIRE(scriptname->content() == "MyScript");
+  const auto &keyword{scriptnameStatement->children[0]};
+  REQUIRE(keyword->id == &typeid(scripting::RawScriptname));
+  const auto &scriptname{scriptnameStatement->children[1]};
+  REQUIRE(scriptname->has_content());
+  REQUIRE(scriptname->content() == "MyScript");
+}
 
-    //scripting::visitAst(*root, std::string{}, printVisitor);
+TEST_CASE("fails to parse invalid scriptname") {
+  {
+    const char *script = "scriptname ;This is a comment";
+    pegtl::memory_input in(script, "");
+    REQUIRE_THROWS_AS(parseScript(in), pegtl::parse_error);
+  }
+
+  {
+    const char *script = "scriptname 12hello";
+    pegtl::memory_input in(script, "");
+    REQUIRE_THROWS_AS(parseScript(in), pegtl::parse_error);
+  }
+
+  {
+    const char *script = "scnmore MyScript";
+    pegtl::memory_input in(script, "");
+    REQUIRE_THROWS_AS(parseScript(in), pegtl::parse_error);
+  }
+
+  {
+    const char *script = "";
+    pegtl::memory_input in(script, "");
+    REQUIRE_THROWS_AS(parseScript(in), pegtl::parse_error);
   }
 }
