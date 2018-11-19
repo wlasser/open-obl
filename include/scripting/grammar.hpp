d10757467aed85e9b1ae7775d3a6864700ee59b9
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-11-19 12:47:56 +0000

    Added block statements

--- include/scripting/grammar.hpp
+++ include/scripting/grammar.hpp
@@ -14,11 +14,24 @@ namespace pegtl = tao::TAO_PEGTL_NAMESPACE;
 /// \name Strings
 /// Literal strings used for constructing keywords, types, etc.
 ///@{
+
+/// `StrScriptname <- "scriptname"`
 struct StrScriptname : pegtl::string<'s', 'c', 'r', 'i', 'p', 't',
                                      'n', 'a', 'm', 'e'> {
 };
+
+/// `StrScn <- "scn"`
 struct StrScn : pegtl::string<'s', 'c', 'n'> {};
+
+/// `StrSemicolon <- ";"`
 struct StrSemicolon : pegtl::one<';'> {};
+
+/// `StrBegin <- "begin"`
+struct StrBegin : pegtl::string<'b', 'e', 'g', 'i', 'n'> {};
+
+/// `StrEnd <- "end"`
+struct StrEnd : pegtl::string<'e', 'n', 'd'> {};
+
 ///@}
 
 /// \name Character classes
@@ -34,51 +47,96 @@ struct InitialIdChar : pegtl::alpha {};
 
 /// \name Spacing
 ///@{
+
 /// `Comment <- ";" (!EndOfLine .)* EndOfLine`
 struct Comment : pegtl::seq<StrSemicolon,
                             pegtl::star<pegtl::seq<pegtl::not_at<pegtl::eolf>,
                                                    pegtl::any>>,
                             pegtl::eolf> {
 };
+
 /// `Spacing <- (Space / Comment)*`
 struct Spacing : pegtl::star<pegtl::sor<pegtl::space, Comment>> {};
-///@}
 
-/// \name Keywords
-/// @{
+///@}
 
 namespace impl {
 
 /// Convenience class template for allowing trailing space.
 /// Keywords, types, and punctuation may all have trailing space, but we do not
-/// want the space to appear in the AST. Thus each of them should have
+/// want the space to appear in the AST.
 template<class Raw>
 struct Spaced : pegtl::seq<Raw, Spacing> {};
 
 }
 
-/// SCRIPTNAME <- "scriptname"
+/// \name Raw keywords
+///@{
+
+/// `ScriptnameLong <- StrScriptname`
 struct ScriptnameLong : StrScriptname {};
 
-/// SCN <- "scn"
+/// `ScriptnameShort <- StrScn`
 struct ScriptnameShort : StrScn {};
 
-/// @}
-
+/// `RawScriptname <- ScriptnameLong / ScriptnameShort`
 struct RawScriptname : pegtl::sor<ScriptnameLong, ScriptnameShort> {};
-struct RawIdentifier : pegtl::seq<InitialIdChar, pegtl::star<IdChar>> {};
 
+/// `RawBegin <- StrBegin`
+struct RawBegin : StrBegin {};
+
+/// `RawEnd <- StrEnd`
+struct RawEnd : StrEnd {};
+
+///@}
+
+/// \name Spaced keywords
+///@{
+
+/// `Scriptname <- RawScriptname Spacing`
 struct Scriptname : impl::Spaced<RawScriptname> {};
+
+/// `Begin <- RawBegin Spacing`
+struct Begin : impl::Spaced<RawBegin> {};
+
+/// `End <- RawEnd Spacing`
+struct End : impl::Spaced<RawEnd> {};
+
+///@}
+
+/// `RawIdentifier <- InitialIdChar IdChar*
+struct RawIdentifier : pegtl::seq<InitialIdChar, pegtl::star<IdChar>> {};
+/// `Identifier <- RawIdentifier Spacing`
 struct Identifier : impl::Spaced<RawIdentifier> {};
 
+/// \name Statements
+///@{
+
+/// `RawScriptnameStatement <- Scriptname Identifier`
 struct RawScriptnameStatement : pegtl::seq<Scriptname, Identifier> {};
+
+/// `ScriptnameStatement <- RawScriptnameStatement (Spacing / eolf)`
 struct ScriptnameStatement : pegtl::seq<RawScriptnameStatement,
                                         pegtl::sor<Spacing, pegtl::eolf>> {
 };
 
+struct BlockBeginStatement : pegtl::seq<Begin,
+                                        Identifier
+    /* [IntegerLiteral]? */> {
+};
+
+struct BlockEndStatement : End {};
+
+struct BlockStatement : pegtl::seq<BlockBeginStatement,
+    /* (Statement)* */
+                                   BlockEndStatement> {
+};
+
+///@}
+
 struct Grammar : pegtl::must<Spacing,
                              ScriptnameStatement,
-    //pegtl::star<Block>,
+                             pegtl::star<BlockStatement>,
                              pegtl::eof> {
 };
 
@@ -87,6 +145,8 @@ template<class Rule> struct AstSelector : std::false_type {};
 template<> struct AstSelector<RawScriptnameStatement> : std::true_type {};
 template<> struct AstSelector<RawScriptname> : std::true_type {};
 template<> struct AstSelector<RawIdentifier> : std::true_type {};
+template<> struct AstSelector<BlockBeginStatement> : std::true_type {};
+template<> struct AstSelector<BlockEndStatement> : std::true_type {};
 
 template<class F, class State>
 void visitAst(const pegtl::parse_tree::node &node, State state, F &&visitor) {
--- test/scripting/grammar.cpp
+++ test/scripting/grammar.cpp
@@ -1,10 +1,13 @@
 #include "scripting/grammar.hpp"
 #include <catch2/catch.hpp>
 #include <tao/pegtl/analyze.hpp>
-#include <tao/pegtl/contrib/parse_tree.hpp>
-#include <iostream>
 
 namespace pegtl = tao::TAO_PEGTL_NAMESPACE;
+
+[[nodiscard]] bool isStatement(const pegtl::parse_tree::node &node) {
+  const std::string_view name{node.id->name()};
+  return name.find("Statement") != std::string_view::npos;
+}
 
 [[nodiscard]] std::string
 printVisitor(const pegtl::parse_tree::node &node, const std::string &indent) {
@@ -12,7 +15,9 @@ printVisitor(const pegtl::parse_tree::node &node, const std::string &indent) {
     std::cout << "ROOT\n";
   } else {
     std::cout << indent << node.name();
-    if (node.has_content()) std::cout << " \"" << node.content() << '"';
+    if (node.has_content() && !isStatement(node)) {
+      std::cout << " \"" << node.content() << '"';
+    }
     std::cout << " at " << node.begin() << " to " << node.end() << '\n';
   }
   return indent + "| ";
@@ -48,13 +53,14 @@ TEST_CASE("can parse scriptname", "[scripting]") {
     const char *script = R"script(
 ; First comment
  scn MyScript ; This is the script name
+   ; Script body goes here!
     )script";
     pegtl::memory_input in(script, "");
     root = parseScript(in);
   }
 
   REQUIRE(root != nullptr);
-  REQUIRE(!root->children.empty());
+  REQUIRE_FALSE(root->children.empty());
 
   const auto &scriptnameStatement{root->children[0]};
   REQUIRE(scriptnameStatement->children.size() > 1);
@@ -66,7 +72,10 @@ TEST_CASE("can parse scriptname", "[scripting]") {
   REQUIRE(scriptname->content() == "MyScript");
 }
 
-TEST_CASE("fails to parse invalid scriptname") {
+TEST_CASE("fails to parse invalid scriptname", "[scripting]") {
+  const auto numIssues{pegtl::analyze<scripting::Grammar>()};
+  REQUIRE(numIssues == 0);
+
   {
     const char *script = "scriptname ;This is a comment";
     pegtl::memory_input in(script, "");
@@ -87,6 +96,147 @@ TEST_CASE("fails to parse invalid scriptname") {
 
   {
     const char *script = "";
+    pegtl::memory_input in(script, "");
+    REQUIRE_THROWS_AS(parseScript(in), pegtl::parse_error);
+  }
+}
+
+TEST_CASE("can parse block statements", "[scripting]") {
+  const auto numIssues{pegtl::analyze<scripting::Grammar>()};
+  REQUIRE(numIssues == 0);
+
+  std::unique_ptr<pegtl::parse_tree::node> root{};
+  std::string expectedBlockname{"GameMode"};
+
+  SECTION("parse minimal block statement") {
+    const char *script = R"script(
+scn MyScript
+
+begin GameMode
+end
+)script";
+    pegtl::memory_input in(script, "");
+    root = parseScript(in);
+  }
+
+  SECTION("parse block statement with surrounding comments") {
+    const char *script = R"script(
+scn MyScript
+begin   GameMode ; Runs during gameplay
+; This is where I'd put my script, if I had one
+     end ; End of script
+    )script";
+    pegtl::memory_input in(script, "");
+    root = parseScript(in);
+  }
+
+  SECTION("parse block statement with unexpected name") {
+    const char *script = R"script(
+scn MyScript
+begin begin
+end
+)script";
+    pegtl::memory_input in(script, "");
+    root = parseScript(in);
+    expectedBlockname = "begin";
+  }
+
+  REQUIRE(root != nullptr);
+  REQUIRE(root->children.size() > 2);
+
+  const auto &beginStatement{root->children[1]};
+  REQUIRE(beginStatement->id == &typeid(scripting::BlockBeginStatement));
+  REQUIRE_FALSE(beginStatement->children.empty());
+
+  const auto &blockName{beginStatement->children[0]};
+  REQUIRE(blockName->has_content());
+  REQUIRE(blockName->content() == expectedBlockname);
+
+  const auto &endStatement{root->children[2]};
+  REQUIRE(endStatement->id == &typeid(scripting::BlockEndStatement));
+}
+
+TEST_CASE("can parse multiple block statements", "[scripting]") {
+  const auto numIssues{pegtl::analyze<scripting::Grammar>()};
+  REQUIRE(numIssues == 0);
+
+  std::unique_ptr<pegtl::parse_tree::node> root{};
+
+  SECTION("parse nicely formatted blocks") {
+    const auto *script = R"script(
+scn MyScript
+
+; First block
+begin GameMode
+end
+
+; Next block
+begin MenuMode
+end
+)script";
+    pegtl::memory_input in(script, "");
+    root = parseScript(in);
+  }
+
+  SECTION("parse poorly formatted blocks") {
+    const auto *script = R"script(
+scn MyScript begin GameMode end begin MenuMode
+end
+)script";
+    pegtl::memory_input in(script, "");
+    root = parseScript(in);
+  }
+
+  REQUIRE(root != nullptr);
+  REQUIRE(root->children.size() > 4);
+
+  {
+    const auto &beginStatement{root->children[1]};
+    REQUIRE(beginStatement->id == &typeid(scripting::BlockBeginStatement));
+    REQUIRE_FALSE(beginStatement->children.empty());
+
+    const auto &blockName{beginStatement->children[0]};
+    REQUIRE(blockName->has_content());
+    REQUIRE(blockName->content() == "GameMode");
+
+    const auto &endStatement{root->children[2]};
+    REQUIRE(endStatement->id == &typeid(scripting::BlockEndStatement));
+  }
+
+  {
+    const auto &beginStatement{root->children[3]};
+    REQUIRE(beginStatement->id == &typeid(scripting::BlockBeginStatement));
+    REQUIRE_FALSE(beginStatement->children.empty());
+
+    const auto &blockName{beginStatement->children[0]};
+    REQUIRE(blockName->has_content());
+    REQUIRE(blockName->content() == "MenuMode");
+
+    const auto &endStatement{root->children[4]};
+    REQUIRE(endStatement->id == &typeid(scripting::BlockEndStatement));
+  }
+}
+
+TEST_CASE("fails to parse invalid block statements", "[scripting]") {
+  const auto numIssues{pegtl::analyze<scripting::Grammar>()};
+  REQUIRE(numIssues == 0);
+
+  {
+    const char *script = R"script(
+scn MyScript
+begin ; No block name
+end
+)script";
+    pegtl::memory_input in(script, "");
+    REQUIRE_THROWS_AS(parseScript(in), pegtl::parse_error);
+  }
+
+  {
+    const char *script = R"script(
+scn MyScript
+begin begin begin
+end
+    )script";
     pegtl::memory_input in(script, "");
     REQUIRE_THROWS_AS(parseScript(in), pegtl::parse_error);
   }
