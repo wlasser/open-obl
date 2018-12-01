b54b7ba901e96b8910cbab0a32759c67caba02ed
Daniel Mansfield <daniel.mansfield99@gmail.com>
2018-12-01 23:31:44 +0000

    Made stack:: explicit in stack tests

--- test/gui/stack.cpp
+++ test/gui/stack.cpp
@@ -6,47 +6,47 @@
 #include <string>
 #include <variant>
 
-namespace gui::stack {
+namespace gui {
 
 TEST_CASE("parseValueType deduces bool", "[gui][gui/stack]") {
   {
-    const auto r{parseValueType("&true;")};
+    const auto r{stack::parseValueType("&true;")};
     REQUIRE(std::holds_alternative<bool>(r));
     REQUIRE(std::get<bool>(r));
   }
 
   {
-    const auto r{parseValueType("&false;")};
+    const auto r{stack::parseValueType("&false;")};
     REQUIRE(std::holds_alternative<bool>(r));
     REQUIRE_FALSE(std::get<bool>(r));
   }
 
   {
-    const auto r{parseValueType("true")};
+    const auto r{stack::parseValueType("true")};
     REQUIRE_FALSE(std::holds_alternative<bool>(r));
   }
 
   {
-    const auto r{parseValueType("1")};
+    const auto r{stack::parseValueType("1")};
     REQUIRE_FALSE(std::holds_alternative<bool>(r));
   }
 }
 
 TEST_CASE("parseValueType deduces int", "[gui][gui/stack]") {
   {
-    const auto r{parseValueType("1")};
+    const auto r{stack::parseValueType("1")};
     REQUIRE(std::holds_alternative<int>(r));
     REQUIRE(std::get<int>(r) == 1);
   }
 
   {
-    const auto r{parseValueType("-10")};
+    const auto r{stack::parseValueType("-10")};
     REQUIRE(std::holds_alternative<int>(r));
     REQUIRE(std::get<int>(r) == -10);
   }
 
   {
-    const auto r{parseValueType("0")};
+    const auto r{stack::parseValueType("0")};
     REQUIRE(std::holds_alternative<int>(r));
     REQUIRE(std::get<int>(r) == 0);
   }
@@ -54,23 +54,23 @@ TEST_CASE("parseValueType deduces int", "[gui][gui/stack]") {
 
 TEST_CASE("parseValueType deduces float", "[gui][gui/stack]") {
   {
-    const auto r{parseValueType("3.14")};
+    const auto r{stack::parseValueType("3.14")};
     REQUIRE(std::holds_alternative<float>(r));
     REQUIRE_THAT(std::get<float>(r), Catch::WithinULP(3.14f, 1));
   }
 
   {
-    const auto r{parseValueType("-2.0")};
+    const auto r{stack::parseValueType("-2.0")};
     REQUIRE(std::holds_alternative<float>(r));
     REQUIRE_THAT(std::get<float>(r), Catch::WithinULP(-2.0f, 1));
   }
 }
 
 TEST_CASE("can push values onto the stack", "[gui][gui/stack]") {
-  Program program{};
-  program.instructions = std::vector<Instruction>{
-      push_t{10},
-      push_t{15}
+  stack::Program program{};
+  program.instructions = std::vector<stack::Instruction>{
+      stack::push_t{10},
+      stack::push_t{15}
   };
 
   auto ret{program()};
@@ -79,17 +79,17 @@ TEST_CASE("can push values onto the stack", "[gui][gui/stack]") {
 }
 
 TEST_CASE("can perform integer arithmetic on the stack", "[gui][gui/stack]") {
-  Program program{};
-  program.instructions = std::vector<Instruction>{
-      push_t{5},
-      push_t{10},
-      add_t{},
-      push_t{3},
-      div_t{},
-      push_t{2},
-      mul_t{},
-      push_t{3},
-      mod_t{}
+  stack::Program program{};
+  program.instructions = std::vector<stack::Instruction>{
+      stack::push_t{5},
+      stack::push_t{10},
+      stack::add_t{},
+      stack::push_t{3},
+      stack::div_t{},
+      stack::push_t{2},
+      stack::mul_t{},
+      stack::push_t{3},
+      stack::mod_t{}
   };
 
   auto ret{program()};
@@ -99,15 +99,15 @@ TEST_CASE("can perform integer arithmetic on the stack", "[gui][gui/stack]") {
 
 TEST_CASE("can perform floating point arithmetic on the stack",
           "[gui][gui/stack") {
-  Program program{};
-  program.instructions = std::vector<Instruction>{
-      push_t{3.1f},
-      push_t{6.9f},
-      add_t{},
-      push_t{2.5f},
-      div_t{},
-      push_t{0.7f},
-      mul_t{}
+  stack::Program program{};
+  program.instructions = std::vector<stack::Instruction>{
+      stack::push_t{3.1f},
+      stack::push_t{6.9f},
+      stack::add_t{},
+      stack::push_t{2.5f},
+      stack::div_t{},
+      stack::push_t{0.7f},
+      stack::mul_t{}
   };
 
   auto ret{program()};
@@ -117,15 +117,15 @@ TEST_CASE("can perform floating point arithmetic on the stack",
 }
 
 TEST_CASE("can nop and retain stack state", "[gui][gui/stack]") {
-  Program program{};
-  program.instructions = std::vector<Instruction>{
-      push_t{10},
-      nop_t{},
-      nop_t{},
-      push_t{5},
-      nop_t{},
-      add_t{},
-      nop_t{}
+  stack::Program program{};
+  program.instructions = std::vector<stack::Instruction>{
+      stack::push_t{10},
+      stack::nop_t{},
+      stack::nop_t{},
+      stack::push_t{5},
+      stack::nop_t{},
+      stack::add_t{},
+      stack::nop_t{}
   };
 
   auto ret{program()};
@@ -134,33 +134,33 @@ TEST_CASE("can nop and retain stack state", "[gui][gui/stack]") {
 }
 
 TEST_CASE("can compute gcd and lcm on the stack", "[gui][gui/stack]") {
-  Program p_not_coprime{};
-  p_not_coprime.instructions = std::vector<Instruction>{
-      push_t{100},
-      push_t{128},
-      gcd_t{}
+  stack::Program p_not_coprime{};
+  p_not_coprime.instructions = std::vector<stack::Instruction>{
+      stack::push_t{100},
+      stack::push_t{128},
+      stack::gcd_t{}
   };
 
   auto r_not_coprime{p_not_coprime()};
   REQUIRE(std::holds_alternative<int>(r_not_coprime));
   REQUIRE(std::get<int>(r_not_coprime) == 4);
 
-  Program p_coprime{};
-  p_coprime.instructions = std::vector<Instruction>{
-      push_t{79},
-      push_t{25},
-      gcd_t{}
+  stack::Program p_coprime{};
+  p_coprime.instructions = std::vector<stack::Instruction>{
+      stack::push_t{79},
+      stack::push_t{25},
+      stack::gcd_t{}
   };
 
   auto r_coprime{p_coprime()};
   REQUIRE(std::holds_alternative<int>(r_coprime));
   REQUIRE(std::get<int>(r_coprime) == 1);
 
-  Program p_lcm{};
-  p_lcm.instructions = std::vector<Instruction>{
-      push_t{100},
-      push_t{128},
-      lcm_t{}
+  stack::Program p_lcm{};
+  p_lcm.instructions = std::vector<stack::Instruction>{
+      stack::push_t{100},
+      stack::push_t{128},
+      stack::lcm_t{}
   };
 
   auto r_lcm{p_lcm()};
@@ -169,13 +169,13 @@ TEST_CASE("can compute gcd and lcm on the stack", "[gui][gui/stack]") {
 }
 
 TEST_CASE("can compute floor and ceiling on the stack", "[gui][gui/stack]") {
-  Program program{};
-  program.instructions = std::vector<Instruction>{
-      push_t{7.5f},
-      push_t{0.0f},
-      floor_t{},
-      push_t{6.7f},
-      ceil_t{}
+  stack::Program program{};
+  program.instructions = std::vector<stack::Instruction>{
+      stack::push_t{7.5f},
+      stack::push_t{0.0f},
+      stack::floor_t{},
+      stack::push_t{6.7f},
+      stack::ceil_t{}
   };
 
   auto ret{program()};
@@ -184,22 +184,22 @@ TEST_CASE("can compute floor and ceiling on the stack", "[gui][gui/stack]") {
 }
 
 TEST_CASE("can compute min and max on the stack", "[gui][gui/stack]") {
-  Program p_min{};
-  p_min.instructions = std::vector<Instruction>{
-      push_t{6.5f},
-      push_t{3.9f},
-      min_t{}
+  stack::Program p_min{};
+  p_min.instructions = std::vector<stack::Instruction>{
+      stack::push_t{6.5f},
+      stack::push_t{3.9f},
+      stack::min_t{}
   };
 
   auto r_min{p_min()};
   REQUIRE(std::holds_alternative<float>(r_min));
   REQUIRE(std::get<float>(r_min) == 3.9f);
 
-  Program p_max{};
-  p_max.instructions = std::vector<Instruction>{
-      push_t{6.5f},
-      push_t{3.9f},
-      max_t{}
+  stack::Program p_max{};
+  p_max.instructions = std::vector<stack::Instruction>{
+      stack::push_t{6.5f},
+      stack::push_t{3.9f},
+      stack::max_t{}
   };
 
   auto r_max{p_max()};
@@ -208,11 +208,11 @@ TEST_CASE("can compute min and max on the stack", "[gui][gui/stack]") {
 }
 
 TEST_CASE("can compute abs on the stack", "[gui][gui/stack]") {
-  Program program{};
-  program.instructions = std::vector<Instruction>{
-      push_t{-7.5f},
-      push_t{1.0f},
-      abs_t{}
+  stack::Program program{};
+  program.instructions = std::vector<stack::Instruction>{
+      stack::push_t{-7.5f},
+      stack::push_t{1.0f},
+      stack::abs_t{}
   };
 
   auto ret{program()};
@@ -221,24 +221,24 @@ TEST_CASE("can compute abs on the stack", "[gui][gui/stack]") {
 }
 
 TEST_CASE("can do comparisons on the stack", "[gui][gui/stack]") {
-  Program program{};
-  program.instructions = std::vector<Instruction>{
-      push_t{3},
-      push_t{5},
-      lt_t{},
-      push_t{std::string{"world"}},
-      push_t{std::string{"hello"}},
-      gt_t{},
-      push_t{3.1f},
-      push_t{3.2f},
-      eq_t{},
-      not_t{},
-      push_t{5},
-      push_t{3},
-      neq_t{},
-      and_t{},
-      and_t{},
-      and_t{}
+  stack::Program program{};
+  program.instructions = std::vector<stack::Instruction>{
+      stack::push_t{3},
+      stack::push_t{5},
+      stack::lt_t{},
+      stack::push_t{std::string{"world"}},
+      stack::push_t{std::string{"hello"}},
+      stack::gt_t{},
+      stack::push_t{3.1f},
+      stack::push_t{3.2f},
+      stack::eq_t{},
+      stack::not_t{},
+      stack::push_t{5},
+      stack::push_t{3},
+      stack::neq_t{},
+      stack::and_t{},
+      stack::and_t{},
+      stack::and_t{}
   };
 
   auto ret{program()};
@@ -247,20 +247,20 @@ TEST_CASE("can do comparisons on the stack", "[gui][gui/stack]") {
 }
 
 TEST_CASE("can branch on the stack", "[gui][gui/stack]") {
-  Program program{};
-  program.instructions = std::vector<Instruction>{
-      push_t{0},    // Zero buffer
-      push_t{0},
-      push_t{3},    // Desired final value
-      push_t{5},
-      push_t{5},
-      eq_t{},       // == true
-      onlyif_t{},   // Should just be '3'
-      push_t{7},    // Want this to be discarded
-      push_t{5},
-      push_t{5},
-      eq_t{},       // == true
-      onlyifnot_t{} // Should discard the 'true' and '7'
+  stack::Program program{};
+  program.instructions = std::vector<stack::Instruction>{
+      stack::push_t{0},    // Zero buffer
+      stack::push_t{0},
+      stack::push_t{3},    // Desired final value
+      stack::push_t{5},
+      stack::push_t{5},
+      stack::eq_t{},       // == true
+      stack::onlyif_t{},   // Should just be '3'
+      stack::push_t{7},    // Want this to be discarded
+      stack::push_t{5},
+      stack::push_t{5},
+      stack::eq_t{},       // == true
+      stack::onlyifnot_t{} // Should discard the 'true' and '7'
   };
 
   auto ret{program()};
@@ -268,4 +268,4 @@ TEST_CASE("can branch on the stack", "[gui][gui/stack]") {
   REQUIRE(std::get<int>(ret) == 3);
 }
 
-} // namespace gui::stack
+} // namespace gui
