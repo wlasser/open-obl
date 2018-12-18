64a4a00df1c0738f0bd1aceb3a2becf4974595d5
Emily Mansfield <daniel.mansfield99@gmail.com>
2018-12-18 15:18:03 +0000

    Gui shader should not use vertex colours

--- shaders/gui_fs.glsl
+++ shaders/gui_fs.glsl
@@ -1,11 +1,10 @@
 #version 330 core
 in vec2 TexCoord;
-in vec4 VertexCol;
 
 uniform sampler2D diffuseMap;
 
 out vec4 FragColor;
 
 void main() {
-    FragColor = texture2D(diffuseMap, TexCoord) * VertexCol;
+    FragColor = texture2D(diffuseMap, TexCoord);
 }
--- shaders/gui_vs.glsl
+++ shaders/gui_vs.glsl
@@ -4,12 +4,10 @@ in vec4 colour;
 in vec4 uv0;
 
 out vec2 TexCoord;
-out vec4 VertexCol;
 
 uniform mat4 worldViewProj;
 
 void main() {
     gl_Position = worldViewProj * vec4(vertex, 0.0f, 1.0f);
-    VertexCol = colour;
     TexCoord = uv0.xy;
 }
