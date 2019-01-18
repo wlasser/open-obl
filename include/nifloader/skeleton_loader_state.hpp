661a0b03b3fdb889988fdeb975bc12f9c301ac7d
Emily Mansfield <daniel.mansfield99@gmail.com>
2019-01-18 17:36:26 +0000

    Add SkeletonLoaderVisitor
    Hopefully this can load skeletons, but there's no real way of testing
    that yet until all the rest of the animation framework is in place, or
    at least until we have some skeletons to actually load.

--- include/nifloader/skeleton_loader_state.hpp
+++ include/nifloader/skeleton_loader_state.hpp
@@ -3,17 +3,46 @@
 
 #include "nifloader/loader.hpp"
 #include "nifloader/loader_state.hpp"
+#include "settings.hpp"
 #include <Ogre.h>
 
 namespace oo {
 
 class SkeletonLoaderState {
- private:
-  oo::BlockGraph mBlocks{};
-  Ogre::Skeleton *mSkeleton{};
-
  public:
   explicit SkeletonLoaderState(Ogre::Skeleton *skeleton, oo::BlockGraph blocks);
+};
+
+struct SkeletonVisitor {
+  using Graph = oo::BlockGraph;
+  using vertex_descriptor = Graph::vertex_descriptor;
+  using edge_descriptor = Graph::edge_descriptor;
+
+  void start_vertex(vertex_descriptor, const Graph &);
+  void discover_vertex(vertex_descriptor, const Graph &);
+  void finish_vertex(vertex_descriptor, const Graph &);
+
+  [[maybe_unused]] void initialize_vertex(vertex_descriptor, const Graph &) {}
+  [[maybe_unused]] void examine_edge(edge_descriptor, const Graph &) {}
+  [[maybe_unused]] void tree_edge(edge_descriptor, const Graph &) {}
+  [[maybe_unused]] void back_edge(edge_descriptor, const Graph &) {}
+  [[maybe_unused]] void forward_or_cross_edge(edge_descriptor, const Graph &) {}
+  [[maybe_unused]] void finish_edge(edge_descriptor, const Graph &) {}
+
+  explicit SkeletonVisitor(Ogre::Skeleton *skeleton)
+      : mSkeleton(skeleton), mLogger(spdlog::get(oo::LOG)) {}
+
+ private:
+  Ogre::Skeleton *mSkeleton{};
+  Ogre::Matrix4 mTransform{Ogre::Matrix4::IDENTITY};
+  Ogre::Bone *mParentBone{};
+  bool mIsSkeleton{false};
+  std::shared_ptr<spdlog::logger> mLogger{};
+
+  void discover_vertex(const nif::NiNode &node, const Graph &g);
+  void discover_vertex(const nif::BSXFlags &bsxFlags, const Graph &g);
+
+  void finish_vertex(const nif::NiNode &node, const Graph &g);
 };
 
 } // namespace oo
--- src/nifloader/skeleton_loader_state.cpp
+++ src/nifloader/skeleton_loader_state.cpp
@@ -1,11 +1,81 @@
+#include "conversions.hpp"
 #include "nifloader/skeleton_loader_state.hpp"
+#include <boost/graph/depth_first_search.hpp>
+#include <vector>
 
 namespace oo {
 
 SkeletonLoaderState::SkeletonLoaderState(Ogre::Skeleton *skeleton,
-                                         oo::BlockGraph blocks)
-    : mBlocks(blocks), mSkeleton(skeleton) {
+                                         oo::BlockGraph blocks) {
+  std::vector<boost::default_color_type> colorMap(boost::num_vertices(blocks));
+  const auto indexMap{boost::get(boost::vertex_index, blocks)};
+  const auto propertyMap{boost::make_iterator_property_map(colorMap.begin(),
+                                                           indexMap)};
+  SkeletonVisitor visitor(skeleton);
+  boost::depth_first_search(blocks, std::move(visitor), propertyMap);
+}
 
+void SkeletonVisitor::start_vertex(SkeletonVisitor::vertex_descriptor,
+                                   const SkeletonVisitor::Graph &) {
+  mTransform = Ogre::Matrix4::IDENTITY;
+}
+
+void SkeletonVisitor::discover_vertex(SkeletonVisitor::vertex_descriptor v,
+                                      const SkeletonVisitor::Graph &g) {
+  const auto &niObject{*g[v]};
+
+  if (dynamic_cast<const nif::NiNode *>(&niObject)) {
+    discover_vertex(dynamic_cast<const nif::NiNode &>(niObject), g);
+  } else if (dynamic_cast<const nif::BSXFlags *>(&niObject)) {
+    discover_vertex(dynamic_cast<const nif::BSXFlags &>(niObject), g);
+  }
+}
+
+void SkeletonVisitor::finish_vertex(SkeletonVisitor::vertex_descriptor v,
+                                    const SkeletonVisitor::Graph &g) {
+  const auto &niObject{*g[v]};
+
+  if (dynamic_cast<const nif::NiNode *>(&niObject)) {
+    finish_vertex(dynamic_cast<const nif::NiNode &>(niObject), g);
+  }
+}
+
+void SkeletonVisitor::discover_vertex(const nif::BSXFlags &bsxFlags,
+                                      const SkeletonVisitor::Graph &) {
+  using Flags = nif::BSXFlags::Flags;
+  const Flags flags{bsxFlags.data};
+  if ((flags & Flags::bRagdoll) != Flags::bNone) {
+    mIsSkeleton = true;
+  }
+}
+
+void SkeletonVisitor::discover_vertex(const nif::NiNode &node,
+                                      const SkeletonVisitor::Graph &) {
+  mTransform = mTransform * oo::getTransform(node);
+  if (!mIsSkeleton) return;
+
+  // All NiNodes after the root are assumed to be bones
+  auto *bone{mSkeleton->createBone(node.name.str())};
+  if (mParentBone) mParentBone->addChild(bone);
+  mParentBone = bone;
+
+  bone->setInheritOrientation(true);
+  bone->setInheritScale(true);
+
+  bone->setPosition(oo::fromBSCoordinates(oo::fromNif(node.translation)));
+  bone->setScale(node.scale, node.scale, node.scale);
+  bone->setOrientation(oo::fromBSCoordinates(fromNif(node.rotation)));
+
+  bone->setBindingPose();
+}
+
+void SkeletonVisitor::finish_vertex(const nif::NiNode &node,
+                                    const SkeletonVisitor::Graph &) {
+  mTransform = mTransform * oo::getTransform(node).inverse();
+
+  if (mIsSkeleton) {
+    mParentBone = dynamic_cast<Ogre::Bone *>(mParentBone->getParent());
+  }
 }
 
 } // namespace oo
