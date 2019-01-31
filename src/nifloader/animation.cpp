47cb09bcde002b6d6f13cd5298a204601ebc8a5a
Emily Mansfield <daniel.mansfield99@gmail.com>
2019-01-31 10:06:26 +0000

    Continue refactoring of oo::createAnimation

--- src/nifloader/animation.cpp
+++ src/nifloader/animation.cpp
@@ -84,6 +84,20 @@ std::string getBoneName(const BlockGraph &graph,
   return std::string(p.palette.value.data() + nodeNameOffset);
 }
 
+std::string getBoneName(const BlockGraph &graph,
+                        const nif::compound::ControlledBlock &block) {
+  if (block.palette) {
+    return oo::getBoneName(graph, *block.palette);
+    // TODO: Support properties, controllers, etc.
+  } else if (block.idTag) {
+    // TODO: Support 10.1.0.104 <= ver <= 10.1.0.113
+    return "";
+  } else {
+    // TODO: Support ver < 10.1.0.104
+    return block.targetName->str();
+  }
+}
+
 // TODO: Support older interpolator versions
 const nif::NiTransformInterpolator &
 getInterpolator(const BlockGraph &graph,
@@ -105,6 +119,41 @@ getInterpolator(const BlockGraph &graph,
               "oo::createAnimation");
 }
 
+Ogre::Vector3 getTranslation(const nif::compound::NiQuatTransform &trans) {
+  if (!trans.trsValid || trans.trsValid->at(0)) {
+    return oo::fromBSCoordinates(oo::fromNif(trans.translation));
+  }
+  return Ogre::Vector3::ZERO;
+}
+
+Ogre::Vector3 getScale(const nif::compound::NiQuatTransform &trans) {
+  if (!trans.trsValid || trans.trsValid->at(2)) {
+    return Ogre::Vector3{trans.scale, trans.scale, trans.scale};
+  }
+  return Ogre::Vector3::UNIT_SCALE;
+}
+
+Ogre::Quaternion getRotation(const nif::compound::NiQuatTransform &trans) {
+  if (!trans.trsValid || trans.trsValid->at(1)) {
+    return oo::fromBSCoordinates(oo::fromNif(trans.rotation));
+  }
+  return Ogre::Quaternion::IDENTITY;
+}
+
+const nif::NiTransformData *
+getTransformData(const BlockGraph &graph,
+                 const nif::NiTransformInterpolator &interpolator) {
+  if (!interpolator.data) return nullptr;
+
+  const auto iVal{static_cast<int32_t>(interpolator.data)};
+  if (iVal < 0) return nullptr;
+
+  const auto val{static_cast<std::size_t>(iVal)};
+  if (val >= boost::num_vertices(graph)) return nullptr;
+
+  return dynamic_cast<const nif::NiTransformData *>(&*graph[val]);
+}
+
 } // namespace
 
 Ogre::Animation *createAnimation(Ogre::Skeleton *skeleton,
@@ -122,18 +171,7 @@ Ogre::Animation *createAnimation(Ogre::Skeleton *skeleton,
   // TODO: Support animation priority and weight
 
   for (const auto &block : controller.controlledBlocks) {
-    std::string boneName{};
-
-    if (block.palette) {
-      boneName = oo::getBoneName(blockGraph, *block.palette);
-      // TODO: Support properties, controllers, etc.
-    } else if (block.idTag) {
-      // TODO: Support 10.1.0.104 <= ver <= 10.1.0.113
-    } else {
-      // TODO: Support ver < 10.1.0.104
-      boneName = block.targetName->str();
-    }
-
+    std::string boneName{oo::getBoneName(blockGraph, block)};
     const auto &interpolator{oo::getInterpolator(blockGraph, block)};
 
     // This is the transformation of the bone from the origin to its binding
@@ -143,107 +181,64 @@ Ogre::Animation *createAnimation(Ogre::Skeleton *skeleton,
     // each keyframe.
     const auto &trans{interpolator.transform};
 
-    const Ogre::Vector3 translation = [&]() {
-      if (!trans.trsValid || trans.trsValid->at(0)) {
-        return oo::fromBSCoordinates(oo::fromNif(trans.translation));
-      }
-      return Ogre::Vector3::ZERO;
-    }();
+    const Ogre::Vector3 translation{oo::getTranslation(trans)};
+    const Ogre::Vector3 scale{oo::getScale(trans)};
+    const Ogre::Quaternion rotation{oo::getRotation(trans)};
 
-    const Ogre::Quaternion rotation = [&]() {
-      if (!trans.trsValid || trans.trsValid->at(1)) {
-        return oo::fromBSCoordinates(oo::fromNif(trans.rotation));
-      }
-      return Ogre::Quaternion::IDENTITY;
-    }();
-
-    const Ogre::Vector3 scale = [&]() {
-      if (!trans.trsValid || trans.trsValid->at(2)) {
-        return Ogre::Vector3{trans.scale, trans.scale, trans.scale};
-      }
-      return Ogre::Vector3::UNIT_SCALE;
-    }();
-
+    // getBone throws if the bone doesn't exist, it does not return nullptr.
     if (!skeleton->hasBone(boneName)) continue;
-
     auto *bone{skeleton->getBone(boneName)};
 
-    const nif::NiTransformData *transformData{};
-    if (interpolator.data) {
-      const auto iVal{static_cast<int32_t>(interpolator.data)};
-      if (iVal < 0) return nullptr;
-
-      const auto val{static_cast<std::size_t>(iVal)};
-      if (val >= boost::num_vertices(blockGraph)) return nullptr;
-
-      transformData = dynamic_cast<const nif::NiTransformData *>(
-          &*blockGraph[val]);
-    }
-
-    if (transformData) {
+    if (const auto *data{oo::getTransformData(blockGraph, interpolator)}) {
       // Keyframes are split up by rotation, translation, and scale. They should
       // be ordered by increasing time, but this is not necessary and most
       // (all?) of our data is already ordered so we don't bother with a sort.
 
       // Scale
-      if (transformData->scales.numKeys > 0) {
+      if (data->scales.numKeys > 0) {
         auto *track{anim->createNodeScalingTrack(bone->getHandle(), bone)};
         std::visit([&](const auto &keys) {
           for (const auto &key : keys) {
-            const float time{key.time};
-            auto *kf{track->createScalingKeyFrame(time)};
+            auto *kf{track->createScalingKeyFrame(key.time)};
             const Ogre::Vector3 s{key.value, key.value, key.value};
             kf->setScale(s / scale);
           }
-        }, transformData->scales.keys);
+        }, data->scales.keys);
       }
 
       // Translation
-      if (transformData->translations.numKeys > 0) {
+      if (data->translations.numKeys > 0) {
         auto *track{anim->createNodeTranslationTrack(bone->getHandle(), bone)};
         std::visit([&](const auto &keys) {
           for (const auto &key : keys) {
-            const float time{key.time};
-            auto *kf{track->createTranslationKeyFrame(time)};
+            auto *kf{track->createTranslationKeyFrame(key.time)};
             const auto t{oo::fromBSCoordinates(oo::fromNif(key.value))};
             kf->setTranslate(t - translation);
           }
-        }, transformData->translations.keys);
+        }, data->translations.keys);
       }
 
       // Rotation
-      using KeyType = nif::Enum::KeyType;
-
-      if (transformData->rotationType == KeyType::XYZ_ROTATION_KEY) {
-        OGRE_EXCEPT(Ogre::Exception::ERR_NOT_IMPLEMENTED,
-                    "XYZ rotation keys are not supported",
-                    "oo::createAnimation()");
-      } else {
-        if (transformData->numRotationKeys > 0) {
+      if (data->rotationType != nif::Enum::KeyType::XYZ_ROTATION_KEY) {
+        if (data->numRotationKeys > 0) {
           auto *track{anim->createNodeRotationTrack(bone->getHandle(), bone)};
+          const auto inv{rotation.Inverse()};
           std::visit([&](const auto &keys) {
             for (const auto &key : keys) {
-              const float time{key.time};
-              auto *kf{track->createRotationKeyFrame(time)};
+              auto *kf{track->createRotationKeyFrame(key.time)};
               const auto r{oo::fromBSCoordinates(oo::fromNif(key.value))};
-              kf->setRotation(rotation.Inverse() * r);
+              kf->setRotation(inv * r);
             }
-          }, transformData->quaternionKeys);
+          }, data->quaternionKeys);
         }
+      } else {
+        OGRE_EXCEPT(Ogre::Exception::ERR_NOT_IMPLEMENTED,
+                    "XYZ rotation keys are not supported",
+                    "oo::createAnimation");
       }
-      // End transformData
     } else {
-      auto *track{anim->createNodeTrack(bone->getHandle(), bone)};
-
-      auto *k0{track->createNodeKeyFrame(startTime)};
-      k0->setTranslate(Ogre::Vector3::ZERO);
-      k0->setScale(Ogre::Vector3::UNIT_SCALE);
-      k0->setRotation(Ogre::Quaternion::IDENTITY);
-
-      auto *k1{track->createNodeKeyFrame(stopTime)};
-      k1->setTranslate(Ogre::Vector3::ZERO);
-      k1->setScale(Ogre::Vector3::UNIT_SCALE);
-      k1->setRotation(Ogre::Quaternion::IDENTITY);
+      // In this case I think the Nif would just transform to the binding pose,
+      // but that's the default with Ogre so we don't need to add any frames.
     }
   }
 
