70f1b6c355d7f0363e1446ffffb3d08c9163dd65
Emily Mansfield <daniel.mansfield99@gmail.com>
2019-03-19 16:08:33 +0000

    Recursively set refId on collision objects

--- src/resolvers/helpers.cpp
+++ src/resolvers/helpers.cpp
@@ -67,11 +67,20 @@ void setRefId(gsl::not_null<Ogre::RigidBody *> rigidBody, oo::RefId refId) {
 }
 
 void setRefId(gsl::not_null<Ogre::SceneNode *> node, RefId refId) {
-  for (auto obj : node->getAttachedObjects()) {
-    if (auto *body{dynamic_cast<Ogre::RigidBody *>(obj)}) {
-      oo::setRefId(gsl::make_not_null(body), refId);
+  std::function<void(Ogre::SceneNode *)> dfs = [&](Ogre::SceneNode *root) {
+    for (auto obj : root->getAttachedObjects()) {
+      if (auto *body{dynamic_cast<Ogre::RigidBody *>(obj)}) {
+        oo::setRefId(gsl::make_not_null(body), refId);
+      }
     }
-  }
+    for (Ogre::Node *child : root->getChildren()) {
+      if (auto *sceneChild{dynamic_cast<Ogre::SceneNode *>(child)}) {
+        dfs(sceneChild);
+      }
+    }
+  };
+
+  dfs(node);
 }
 
 } // namespace oo
