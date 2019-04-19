82ab5c3074cfa9916714cde87f6663a4b64e8e0b
Emily Mansfield <daniel.mansfield99@gmail.com>
2019-04-19 12:21:46 +0100

    Fix terrain normalization issue

--- shaders/landscape_low_fs.glsl
+++ shaders/landscape_low_fs.glsl
@@ -22,7 +22,7 @@ void main() {
 
     // We're using the global normal map not any baked normals so no
     // geometric transformation is required, but we do still need to uv flip.
-    gNormal = texture(normalMap, vec2(TexCoord.x, 1.0f - TexCoord.y)).xyz;
+    gNormal = normalize(texture(normalMap, vec2(TexCoord.x, 1.0f - TexCoord.y)).xyz);
 
     gPosition.xyz = FragPos;
     gPosition.w = gl_FragCoord.z;
