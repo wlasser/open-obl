522be9863a80aae0797c46bf07c86b2f584ec7ea
Emily Mansfield <daniel.mansfield99@gmail.com>
2019-04-28 15:27:04 +0100

    Add sunlight to water shahder

--- shaders/post_fs.glsl
+++ shaders/post_fs.glsl
@@ -5,6 +5,10 @@ uniform sampler2D RT;
 out vec4 FragColor;
 
 void main() {
+    vec3 hdrCol = texture(RT, TexCoord).rgb;
+    //    vec3 ldrCol = hdrCol / (vec3(1.0f) + hdrCol);
+    vec3 ldrCol = hdrCol;
+
     float gamma = 2.2f;
-    FragColor = vec4(pow(texture(RT, TexCoord).rgb, vec3(1.0f / gamma)), 1.0f);
+    FragColor = vec4(pow(ldrCol, vec3(1.0f / gamma)), 1.0f);
 }
--- shaders/water_fs.glsl
+++ shaders/water_fs.glsl
@@ -5,6 +5,8 @@ in vec3 ViewPos;
 in vec4 ScreenPos;
 
 uniform vec4 ambientLightColor;
+uniform vec4 sunlightColor;
+uniform vec4 sunlightDirection;
 
 uniform sampler2D diffuse;
 uniform sampler2D Tex0;
@@ -17,33 +19,54 @@ void main() {
     vec4 homScreenPos = ScreenPos / ScreenPos.w;
     vec2 uv = homScreenPos.xy * 0.5f + 0.5f;
 
-    vec3 bump = texture(diffuse, TexCoord).xyz;
+    vec3 lightCol = pow(sunlightColor.rgb, vec3(2.2f));
+    vec3 lightDir = normalize(sunlightDirection.xyz);
 
-    vec4 basePos = texture(Tex0, uv);
-    float baseDepth = clamp(length(FragPos - basePos.xyz) / 5.0f, 0.0f, 1.0f);
+    vec3 shallowCol = vec3(2.0f, 21.0f, 30.0f) / 255.0f * ambientLightColor.rgb;
+    vec3 deepCol = vec3(32.0f, 46.0f, 53.0f) / 255.0f * ambientLightColor.rgb;
+    vec3 reflectCol = vec3(31.0f, 68.0f, 75.0f) / 255.0f * ambientLightColor.rgb;
 
-    vec2 refractedUv = uv + bump.xy * 0.025f * baseDepth;
+    float fresnelAmount = 0.15f;
+    float reflectivityAmount = 0.9f;
 
-    vec4 refractedPos = texture(Tex0, refractedUv);
-    vec3 refractedCol = texture(Tex2, refractedUv).rgb * ambientLightColor.rgb;
+    // Swizzle to make blue component the up component.
+    vec3 normal = normalize(texture(diffuse, TexCoord).xzy);
 
-    if (refractedPos.w < homScreenPos.z) {
-        refractedPos = basePos;
-        refractedCol = texture(Tex2, uv).rgb * ambientLightColor.rgb;
+    vec2 refractedUv = uv + normal.xz * 0.01f;
+
+    vec4 bedPos = texture(Tex0, refractedUv);
+    vec3 bedNormal = texture(Tex1, refractedUv).xyz;
+    vec3 bedAlbedo = texture(Tex2, refractedUv).rgb;
+
+    if (bedPos.w < homScreenPos.z) {
+        bedPos = texture(Tex0, uv);
+        bedNormal = texture(Tex1, uv).xyz;
+        bedAlbedo = texture(Tex2, uv).rgb;
     }
-    float depth = clamp(length(FragPos - refractedPos.xyz) / 5.0f, 0.0f, 1.0f);
+
+    // Apply sunlight diffuse to underwater objects.
+    float diff = max(dot(bedNormal, lightDir), 0.0f);
+    vec3 bedCol = bedAlbedo.rgb * (ambientLightColor.rgb + diff * lightCol.rgb);
+
+    float depth = length(FragPos - bedPos.xyz);
+
+    float ior = 1.3333f;
+    float R0 = pow((1.0f - ior) / (1.0f + ior), 2);
 
     vec3 viewDir = normalize(ViewPos - FragPos);
-    float nDotL = max(dot(viewDir, normalize(bump.xyz)), 0.0f);
-    float facing = 1.0f - nDotL;
-    float fresnelBias = 0.2f;
-    float fresnelExponent = 5.0f;
-    float fresnel = max(mix(pow(facing, fresnelExponent), 1.0f, fresnelBias), 0.0f);
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float nDotH = max(dot(normal, halfwayDir), 0.0f);
+    float R = mix(pow(1.0f - nDotH, 5.0f), 1.0f, R0);
 
-    vec3 baseCol = vec3(0, 0.15, 0.115);
-    vec3 deepBaseCol = vec3(0, 0.065, 0.075);
-    vec3 deepCol = mix(refractedCol, deepBaseCol, depth);
-    vec3 waterCol = mix(baseCol, deepCol, facing);
+    float spec = pow(nDotH, 80.0f);
+    vec3 specCol = fresnelAmount * R * reflectCol
+    + 0.5f * reflectivityAmount * spec * lightCol;
 
-    FragColor = vec4(waterCol, 1.0f);
+    float distScale = clamp(2.0f / depth, 0.0f, 1.0f);
+    vec3 deepWaterCol = mix(deepCol, bedCol, distScale);
+    //    vec3 waterCol = mix(deepWaterCol, shallowCol, nDotH);
+    //    vec3 depthAdjustedCol = mix(waterCol, bedCol, distScale);
+
+    vec3 finalCol = deepWaterCol + specCol;
+    FragColor = vec4(finalCol, 1.0f);
 }
