d0cf168e63a4641942fabe6fa5e343311f7a3468
Emily Mansfield <emily@arlsefell.net>
2019-06-29 12:43:14 +0100

    Remove boost::format dependency in records
    Also remove RecordReadError, which is unused.

--- include/record/exceptions.hpp
+++ include/record/exceptions.hpp
@@ -1,22 +1,17 @@
 #ifndef OPENOBL_RECORD_EXCEPTIONS_HPP
 #define OPENOBL_RECORD_EXCEPTIONS_HPP
 
-#include <boost/format.hpp>
 #include <stdexcept>
 #include <string_view>
 
 namespace record {
 
-struct RecordReadError : virtual std::runtime_error {
-  explicit RecordReadError(std::string_view recordType) :
-      std::runtime_error(boost::str(
-          boost::format("Failed to read %s record") % recordType)) {}
-};
-
 struct RecordNotFoundError : virtual std::runtime_error {
   RecordNotFoundError(std::string_view expected, std::string_view actual) :
-      std::runtime_error(boost::str(
-          boost::format("Expected %s, found %s") % expected % actual)) {}
+      std::runtime_error(std::string("Expected ")
+                             .append(expected)
+                             .append(", found ")
+                             .append(actual)) {}
 };
 
 } // namespace record
