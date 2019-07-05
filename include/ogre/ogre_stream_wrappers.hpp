3641d1c5319fe01a178b8bfd92ed9b0ba72de11e
Emily Mansfield <emily@arlsefell.net>
2019-07-05 15:18:06 +0100

    Improve docs for OGRE stream wrappers

--- include/ogre/ogre_stream_wrappers.hpp
+++ include/ogre/ogre_stream_wrappers.hpp
@@ -7,26 +7,40 @@
 #include <memory>
 #include <type_traits>
 
-// Ogre uses its own Ogre::DataStream class as an alternative to standard
-// streams, arguing that its simpler interface can be implemented for libraries
-// which use io, but are incompatible with standard streams. This is good, but
-// unfortunately means that any library that *does* use standard streams still
-// has to write wrappers into Ogre::DataStream.
-// In the future, it may be a good idea to patch Ogre so that Ogre::DataStream
-// and its subclasses derive from std::istream; then we can avoid these wrappers
-// entirely. A barrier to this is that for standard streams the decision for a
-// stream to be read-only is made at compile-time, whereas it can be made at
-// compile-time in Ogre. Then every stream will have to be an std::iostream, and
-// we will still need wrapper code when passing an std::istream into Ogre.
-// TODO: Convert Ogre to use standard streams?
+/// \file ogre/ogre_stream_wrappers.hpp
+/// \ingroup OpenOBLOgre
+/// Adaptors for working with OGRE streams using a standard stream interface,
+/// and vice-versa.
+///
+/// OGRE uses its own `Ogre::DataStream` class as an alternative to standard
+/// streams, arguing that its simpler interface can be implemented for libraries
+/// which use io, but are incompatible with standard streams. This is good, but
+/// unfortunately means that any library that *does* use standard streams still
+/// has to write wrappers into `Ogre::DataStream`. Unfortunately, these wrappers
+/// come with *a lot* of overhead.
+///
+/// In the future, it may be a good idea to patch OGRE so that
+/// `Ogre::DataStream` and its subclasses derive from `std::istream`; then we
+/// can avoid these wrappers entirely. A barrier to this is that for standard
+/// streams the decision for a stream to be read-only is made at compile-time,
+/// whereas it can be made at runtime in Ogre. Then every stream will have to be
+/// a `std::iostream`, and we will still need wrapper code when passing an
+/// `std::istream` into Ogre.
+
 namespace Ogre {
 
-// OgreDataStreambuf is used to convert an existing Ogre::DataStream into a
-// standard stream. Currently only input is supported.
-// TODO: Support output in OgreDataStreambuf
+/// Convert an existing `Ogre::DataStream` into a standard stream.
+/// Example usage is
+/// ```cpp
+/// Ogre::DataStreamPtr ogreStream = /* ... */;
+/// Ogre::OgreDataStreambuf streambuf(dataStream);
+/// std::istream is(&streambuf);
+/// ```
+/// Currently only input is supported.
+/// \todo Support output in `OgreDataStreambuf`.
 class OgreDataStreambuf : public std::streambuf {
- private:
-  std::shared_ptr<Ogre::DataStream> mOgreStream{};
+ public:
+  explicit OgreDataStreambuf(std::shared_ptr<Ogre::DataStream> ogreDataStream);
 
  protected:
   int_type underflow() override;
@@ -36,35 +50,28 @@ class OgreDataStreambuf : public std::streambuf {
   pos_type seekoff(off_type, std::ios_base::seekdir,
                    std::ios_base::openmode) override;
 
- public:
-  explicit OgreDataStreambuf(std::shared_ptr<Ogre::DataStream> ogreDataStream);
+ private:
+  std::shared_ptr<Ogre::DataStream> mOgreStream{};
 };
 
-// OgreStandardStream is used to convert an existing std::istream into an
-// Ogre::DataStream. Currently only input is supported, and the resulting
-// stream is read-only.
+/// Convert an existing `std::istream` into an `Ogre::DataStream`.
+/// Currently only input is supported, and the resulting stream is read-only.
 template<class T>
 class OgreStandardStream : public Ogre::DataStream {
-  static_assert(std::is_base_of_v<std::istream, T>,
+  static_assert(std::is_convertible_v<T *, std::istream *>,
                 "T must derive from std::istream");
   static_assert(std::is_move_constructible_v<T>,
                 "T must be move constructible");
-
- private:
-  // std::istream::tellg() is not const as it modifies the state bits, but
-  // tell() is required to be const, so this is mutable.
-  mutable T mStream;
-
  public:
   OgreStandardStream(const Ogre::String &name, T &&stream);
   OgreStandardStream(const OgreStandardStream &other) = delete;
   // std::istream move constructor is not noexcept
-  // NOLINTNEXTLINE(performance-noexcept-move-constructor)
+  // NOLINTNEXTLINE(performance-noexcept-move-constructor,hicpp-noexcept-move)
   OgreStandardStream(OgreStandardStream &&other) = default;
   ~OgreStandardStream() override = default;
   OgreStandardStream &operator=(const OgreStandardStream &other) = delete;
-  // std::istream move constructor is not noexcept
-  // NOLINTNEXTLINE(performance-noexcept-move-constructor)
+  // std::istream move assignment operator is not noexcept
+  // NOLINTNEXTLINE(performance-noexcept-move-constructor,hicpp-noexcept-move)
   OgreStandardStream &operator=(OgreStandardStream &&other) = default;
 
   // The underlying standard stream may not be closeable, so this does nothing.
@@ -75,6 +82,11 @@ class OgreStandardStream : public Ogre::DataStream {
   void seek(std::size_t pos) override;
   void skip(long count) override;
   std::size_t tell() const override;
+
+ private:
+  // std::istream::tellg() is not const as it modifies the state bits, but
+  // tell() is required to be const, so this is mutable.
+  mutable T mStream;
 };
 
 template<class T>
