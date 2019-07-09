0620929e9acea7db65222fef0bfc9105be944eeb
Emily Mansfield <emily@arlsefell.net>
2019-07-09 21:46:52 +0100

    Support jobs in EspEdit

--- esptools/esp_editor.cpp
+++ esptools/esp_editor.cpp
@@ -1,7 +1,15 @@
 #include "application.hpp"
+#include "job/job.hpp"
 
 int main(int argc, char *argv[]) {
-  std::unique_ptr<oo::Application>
-      application{std::make_unique<oo::Application>("EspEdit")};
-  application->getRoot()->startRendering();
+  oo::JobManager::start();
+
+  std::unique_ptr<oo::Application> application{};
+
+  oo::RenderJobManager::start([&application]() {
+    application = std::make_unique<oo::Application>("EspEdit");
+    application->getRoot()->startRendering();
+  });
+
+  oo::JobManager::stop();
 }
